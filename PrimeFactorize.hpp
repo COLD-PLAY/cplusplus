5e48c8adc0fe268c854f10dadfdcd87ccd06b9dd
COLD-PLAY <liaozhou98@gmail.com>
2021-01-28 00:01:52 +0800

    prime factorize laptop

---
+++ PrimeFactorize.hpp
@@ -0,0 +1,21 @@
+
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class PrimeFactorize {
+private:
+	const static int mod = 1e9 + 7;
+	vector<int> primes;
+	int n;
+
+public:
+	PrimeFactorize(int _n) : n(_n) {}
+
+	void set(int _n) { n = _n; }
+
+	void getPrimes() {
+		
+	}
+};
